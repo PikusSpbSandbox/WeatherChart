eb31abf5a31c9701ab69956ea323253e62327805
Petr Savelev <petr.savelev.direct@gmail.com>
2022-11-20 04:16:49 +0300

    Angular charts demo as submodule

--- src/app/charts.app.module.ts
+++ src/app/charts.app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { ChartsAppComponent } from './charts.app.component';
 import { ChartsComponent } from './charts/charts.component';
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
