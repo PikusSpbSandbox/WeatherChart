6bf48c69a073ce23346593b941d700a0a782a8d2
Petr Savelev <petr.savelev.direct@gmail.com>
2022-11-20 21:24:59 +0300

    Used angular-responsive-carousel

--- src/app/charts.app.module.ts
+++ src/app/charts.app.module.ts
@@ -1,11 +1,11 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 import { ChartsAppComponent } from './charts.app.component';
 import { ChartsComponent } from './charts/charts.component';
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { HttpClientModule } from "@angular/common/http";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    IvyCarouselModule
   ],
   exports: [
     ChartsAppComponent
